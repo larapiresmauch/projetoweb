467f485f3b8657c5a7270f5ddfa1aebfa416e28a
Lara Pires Mauch <lara.maucg@etec.gov.sp.br>
2024-06-13 14:44:44 -0300

    Criado o gitignore

--- backend/controllers/ClienteControle.js
+++ backend/controllers/ClienteControle.js
@@ -1,5 +1,6 @@
 const Cliente = require('../models/Cliente')
 const bcrypt = require('bcrypt')
+const criarClienteToken = require('../helpers/cria-cliente-token')
 
 module.exports = class ClienteController{
     static async registrar(req, res){
@@ -29,5 +30,26 @@ module.exports = class ClienteController{
             res.status(422).json({mensagem: "Confirme a senha"})
             return
         }
-    }
+
+        /* Verifica se cliente já está cadastrado */
+        const clienteExiste = await Cliente.find0ne({email: email})
+
+        if (clienteExiste){
+            res.status(422).json({mensagem: "E-mail já cadastrado"})
+        }
+
+        /* Crição de senha */
+        const salt = await bcrypt.genSalt(12)
+        const senhaHash = await bcrypt.hash(senha,salt)
+
+        /* Adicionando o cliente ao bd */
+        const cliente = new Cliente({nome, email, telefone, senha: senhaHash})
+
+        try{
+            const novoCliente = await cliente.save()
+            await criarClienteToken(novoCliente, req, res)
+        } catch(erro){
+            res.status(500).json({mensagem: erro})
+        }
+    } /* Fim do método resgitro */
 }
---
+++ backend/helpers/cria-cliente-token.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken')
+const criarClienteToken =
+async(cliente, req, res) =>{
+    const token = jwt.sign({
+        nome: cliente.nome,
+        id: cliente._id
+    }, "myscret")
+
+    /* Retornando token */
+    res.status(200).json({
+        mensagem: "Você está autenticado",
+        token: token,
+        clienteId: cliente._id
+    })
+}
+
+module.exports = criarClienteToken
